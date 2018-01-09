58249aee3c810b8fc2d33279558ce1f8f1a5ceb4
Muthama-Kahohi <paul.kahohi@andela.com>
2018-01-09 12:24:09 +0300

    paul/adds travis and deletes circle ci and updates scheme

--- Scripts/uniquify_projects.sh
+++ Scripts/uniquify_projects.sh
@@ -5,12 +5,15 @@ echo "Usage: uniquify_projects.sh" 1>&2
 exit 1
 fi
 
-SCRIPTS_DIR=$(unset CDPATH && cd "${0%/*}" &>/dev/null && pwd)
+SCRIPTS_DIR=$(unset CDPATH && cd git-ftp  &>/dev/null && pwd)
 MAIN_PROJECT=$(unset CDPATH && cd "$SCRIPTS_DIR"/../FoodApp-ios.xcodeproj &>/dev/null && pwd)
 PODS_PROJECT=$(unset CDPATH && cd "$SCRIPTS_DIR"/../Pods/Pods.xcodeproj &>/dev/null && pwd)
 
 function uniquify_project() {
+
 local PROJECT_DIR=$1
+echo "$PROJECT_DIR"
+
 local PROJECT_FILE="$PROJECT_DIR"/project.pbxproj
 
 if ! xunique -c -p "$PROJECT_FILE" >/dev/null; then
