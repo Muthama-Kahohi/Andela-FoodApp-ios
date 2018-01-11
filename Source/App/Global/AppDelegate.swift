d0f010b679c88cf0e5d79eed0a3e905141bd1cb2
Muthama-Kahohi <paul.kahohi@andela.com>
2018-01-11 15:48:29 +0300

    paul/add new dummy view controller and ensures andela domains only sign in

--- Source/App/Global/AppDelegate.swift
+++ Source/App/Global/AppDelegate.swift
@@ -1,32 +1,19 @@
-//
-//  AppDelegate.swift
-//  FoodApp-ios
-//
-//  Created by Paul Kahohi on 1/8/18.
-//  Copyright © 2018 Paul Kahohi. Andela.
-//
-
 import UIKit
 import GoogleSignIn
 import Firebase
 
 
 @UIApplicationMain
-class AppDelegate: UIResponder, UIApplicationDelegate, GIDSignInDelegate {
+class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
 
         // Use Firebase library to configure APIs
         FirebaseApp.configure()
-
-        //Intialize the sign in
-        guard let FirApp = FirebaseApp.app() else { return false }
-
-        GIDSignIn.sharedInstance().clientID = FirApp.options.clientID
+        
         return true
     }
 
@@ -38,31 +25,5 @@ class AppDelegate: UIResponder, UIApplicationDelegate, GIDSignInDelegate {
                                                  sourceApplication: options[UIApplicationOpenURLOptionsKey.sourceApplication] as? String,
                                                  annotation: options[UIApplicationOpenURLOptionsKey.annotation])
     }
-
-    func sign(_ signIn: GIDSignIn!,
-              didSignInFor user: GIDGoogleUser!,
-              withError error: Error!) {
-
-        if error == nil {
-
-            let userId = user.userID
-            let idToken = user.authentication.idToken
-            let fullName = user.profile.name
-            let givenName = user.profile.givenName
-            let familyName = user.profile.familyName
-            let email = user.profile.email
-
-        } else {
-
-            print ("\(error.localizedDescription)")
-        }
-    }
-
-    func sign(_ signIn: GIDSignIn!,
-              didDisconnectWith user: GIDGoogleUser!,
-              withError error: Error!) {
-        // Any code to be perfomed  when the user disconnects from the app
-    }
-
 }
 
--- Source/App/UI/Base/SignInViewController.swift
+++ Source/App/UI/Base/SignInViewController.swift
@@ -1,19 +1,45 @@
 import UIKit
 import GoogleSignIn
+import Firebase
 
-class SignInViewController: UIViewController, GIDSignInUIDelegate {
+class SignInViewController: UIViewController, GIDSignInUIDelegate, GIDSignInDelegate {
 
     @IBOutlet weak var signIn: GIDSignInButton!
 
     override func viewDidLoad() {
 
         super.viewDidLoad()
+
+        signIn.style = .wide
+
+        //Intialize the sign in
+
+        guard let FirApp = FirebaseApp.app() else { return }
+
+        GIDSignIn.sharedInstance().clientID = FirApp.options.clientID
+        GIDSignIn.sharedInstance().hostedDomain = "andela.com"
         GIDSignIn.sharedInstance().uiDelegate = self
+        GIDSignIn.sharedInstance().delegate = self
     }
 
-    private func updateSigninButton() {
+    func sign(_ signIn: GIDSignIn!,
+              didSignInFor user: GIDGoogleUser!,
+              withError error: Error!) {
 
-        signIn.colorScheme = .dark
+        if let error = error {
+
+            print ("\(error.localizedDescription)")
+        }
+
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+
+        if let viewController = storyboard.instantiateViewController(withIdentifier: "entryViewControllerID") as? EntryViewController {
+            viewController.modalPresentationStyle = .overFullScreen
+            self.present(viewController,
+                         animated: false,
+                         completion: nil)
+        }
     }
+
 }
 
