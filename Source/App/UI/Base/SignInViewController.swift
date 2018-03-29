a86772f214961b51a11ea340433418d47381f511
mungaiandela <martin.mungai@andela.com>
2018-03-29 08:47:03 +0300

    Move signIn to AppDelegate

--- Source/App/UI/Base/SignInViewController.swift
+++ Source/App/UI/Base/SignInViewController.swift
@@ -2,10 +2,11 @@ import UIKit
 import GoogleSignIn
 import Firebase
 
-class SignInViewController: UIViewController, GIDSignInDelegate, GIDSignInUIDelegate {
-    
+class SignInViewController: UIViewController,
+                            GIDSignInUIDelegate {
+        
     // MARK: UIOutlets
-
+    
     @IBOutlet public weak var signIn: GIDSignInButton!
     
     // MARK: Internal Properties
@@ -15,42 +16,20 @@ class SignInViewController: UIViewController, GIDSignInDelegate, GIDSignInUIDele
     // MARK: Public Instance Methods
     
     override public func viewDidLoad() {
-
+        
         super.viewDidLoad()
-
+        
         signIn.style = .wide
-
+        
         //Intialize the sign in
         
-        guard let FirApp = FirebaseApp.app() else { return }
         guard let instance = GIDSignIn.sharedInstance() else { return }
-
-        instance.clientID = FirApp.options.clientID
+        
         instance.hostedDomain = viewModel.domain
         instance.uiDelegate = self
-        instance.delegate = self
         instance.signInSilently()
-    }
-    
-    // MARK: Internal Instance Methods
-
-    internal func sign(_ signIn: GIDSignIn!,
-              didSignInFor user: GIDGoogleUser!,
-              withError error: Error!) {
-
-        if let error = error {
-
-            print ("\(error.localizedDescription)")
-            return
-        }
-
-        let storyboard = UIStoryboard(name: "Main", bundle: nil)
-
-        if let navigationController = storyboard.instantiateViewController(withIdentifier: "navigationController") as? UINavigationController {
-            navigationController.modalPresentationStyle = .overFullScreen
-            self.present(navigationController,
-                         animated: true,
-                         completion: nil)
-        }
+        
     }
 }
+
+
