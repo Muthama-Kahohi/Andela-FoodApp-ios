ec99c421576d066a65833b5f7b9e511233388f89
mungaiandela <martin.mungai@andela.com>
2018-03-29 08:46:32 +0300

    Add signIn

--- Source/App/Global/AppDelegate.swift
+++ Source/App/Global/AppDelegate.swift
@@ -6,21 +6,30 @@ import Firebase
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
+    var currentUserId: String = ""
+    var currentUser: User? = nil
+    let foodAppClient = FoodAppClient()
 
-
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+    func application(_ application: UIApplication,
+                     didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         #if DEBUG
-            guard let firebaseConfig = Bundle.main.path(forResource: "GoogleService-Info-dev", ofType: "plist") else { fatalError("Firebase Configuration not found")}
+            guard
+                let firebaseConfig = Bundle.main.path(forResource: "GoogleService-Info-dev", ofType: "plist")
+                else { fatalError("Firebase Configuration not found")}
         #else
-            guard let firebaseConfig = Bundle.main.path(forResource: "GoogleService-Info", ofType: "plist") else { fatalError("Firebase Configuration not found")}
+            guard
+                let firebaseConfig = Bundle.main.path(forResource: "GoogleService-Info", ofType: "plist")
+                else { fatalError("Firebase Configuration not found")}
         #endif
 
-        guard let options = FirebaseOptions(contentsOfFile: firebaseConfig) else {
-            fatalError("Invalid Firebase configuration file.")
-        }
+        guard
+            let options = FirebaseOptions(contentsOfFile: firebaseConfig)
+            else { fatalError("Invalid Firebase configuration file.")}
 
         FirebaseApp.configure(options: options)
+        GIDSignIn.sharedInstance().clientID = FirebaseApp.app()?.options.clientID
+        GIDSignIn.sharedInstance().delegate = self
         return true
     }
 
@@ -34,10 +43,48 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }
 }
 
-extension Bundle {
+extension AppDelegate: GIDSignInDelegate {
+    func sign(_ signIn: GIDSignIn!,
+              didSignInFor user: GIDGoogleUser!,
+              withError error: Error!) {
+        
+        if let error = error {
+            print("\(error.localizedDescription)")
+            
+        } else {
+            guard
+                let authentication = user.authentication else { return }
+            
+            let credential = GoogleAuthProvider.credential(withIDToken: authentication.idToken,
+                                                           accessToken: authentication.accessToken)
+            
+            Auth.auth().signIn(with: credential, completion: { (user, error) in
+                if let error = error {
+                    print("\(error.localizedDescription)")
+                }
+                if let user = user {
+                    self.currentUser = user
+                    self.currentUserId = user.uid
+                }
+            })
+            
+            let storyboard = UIStoryboard(name: "Main", bundle: nil)
+            
+            if let navigationController = storyboard.instantiateViewController(withIdentifier: "navigationController") as? UINavigationController {
+                navigationController.modalPresentationStyle = .overFullScreen
+                self.window?.rootViewController?.present(navigationController,
+                             animated: true,
+                             completion: nil)
+            }
+        }
+    }
+}
 
+extension Bundle {
     var apiBaseURL: String {
         return object(forInfoDictionaryKey: "serverBaseURL") as? String ?? ""
     }
 }
+
+
 
