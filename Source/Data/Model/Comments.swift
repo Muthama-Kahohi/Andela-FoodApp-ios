049e1d12841409bfa923544f8e2932605468a234
mungaiandela <martin.mungai@andela.com>
2018-04-17 12:09:07 +0300

    Add Comments model

--- Source/Data/Model/Comments.swift
+++ Source/Data/Model/Comments.swift
@@ -1,15 +1,16 @@
 //
-//  Meals.swift
+//  Comments.swift
 //  FoodApp-ios
 //
 //  Created by Martin Mungai on 17/04/2018.
 //  Copyright © 2018 Paul Kahohi. All rights reserved.
 //
 
-public struct Meals {
-
+struct Comments {
+    
+    let comment: String
     let date: String
+    let email: String
     let mealID: String
-    let mealType: String
-    let mealItems: [MealItems]
+    let userID: String
 }
