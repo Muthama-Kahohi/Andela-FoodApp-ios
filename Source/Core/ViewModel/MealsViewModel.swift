417c03113f36098b4e1e3e60d5ab1e7f611ce927
mungaiandela <martin.mungai@andela.com>
2018-04-18 11:59:47 +0300

    Refactor fetch from firebase logic.

--- Source/Core/ViewModel/MealsViewModel.swift
+++ Source/Core/ViewModel/MealsViewModel.swift
@@ -3,72 +3,42 @@ import Foundation
 import FirebaseDatabase
 
 
-class MealsViewModel {
-
-    let cellIdentifier = "mealcell"
-    var uniqueFetchId: Int?
-    var foodList = [MealItem]()
-    var mealnumbers = [String]()
-    var refHandle: UInt = 0
-    var reflist = [DatabaseReference]()
-
-     let ref: DatabaseReference = FoodAppClient.databaseRef
-
-    public func loadSampleMeal(completion: @escaping ([Int]) -> Void){
-
-        // Gets the ref of the meal items of a specific day
-
-        guard
-            let id = uniqueFetchId else { return }
-        
-        let mealsRef = "meals/\(String(id))/meal_items"
-        
-        
-        var list = [Int]()
-        
-        ref.child(mealsRef).observe(.value, with: { snapshot in
-
-            if snapshot.childrenCount > 0 {
-
-                let enumerator = snapshot.children
-                while let next = enumerator.nextObject() as? DataSnapshot, let id = next.value as? Int {
-
-                    list.append(id)
-                }
-            }
-            completion(list)
-        })
-
-    }
-
-    public func populateFoodList(mealIds: [Int], completion: @escaping () -> Void) {
-        let mealItemsRef = "mealItems/"
-
-        for id in mealIds {
-
-            let fullref = "\(mealItemsRef)\(id)"
-            self.ref.child(fullref).observe(.value, with: { snapshot in
-
-                if let snapDict =  snapshot.value as? [String: Any],
-                    let name = snapDict["name"] as? String {
-
-                    let mealItem = MealItem(id: 1,name: name)
-
-                    self.foodList.append(mealItem)
-                     completion()
-                }
-            })
-        }
-    }
+public class MealsViewModel {
     
-    public func getMealType(completion: @escaping(String) -> Void) {
+    let cellIdentifier = "mealcell"
+    var foodList: [MealItems] = []
+    var mealID: String?
+    var mealType: String?
+    var uniqueFetchID: String?
+    
+    let ref: DatabaseReference = FoodAppClient.databaseRef
+    
+    public func populateFoodList(completion: @escaping () -> Void) {
         
-        guard let id = uniqueFetchId else { return }
-        let mealTypeRef = "meals/\(String(id))/meal_type"
+        let mealsRef = "meals/"
+//        let today = Date()
+//        let date = today.dateToIsoFormat(date: today)
+        let date = "2018-03-09" // temporary date
         
-        self.ref.child(mealTypeRef).observeSingleEvent(of: .value) { (snapshot) in
-            if let mealType = snapshot.value as? String {
-                completion(mealType)
+        guard
+            let mealType = uniqueFetchID else { return }
+        
+        let fullref = "\(mealsRef)\(date)/\(mealType)"
+        
+        ref.child(fullref).observe(.value) { (snapshot) in
+            if let mealTypeDict = snapshot.value as? [String: Any],
+                let mealID = mealTypeDict["meal_id"] as? String,
+                let mealItems = mealTypeDict["meal_items"] as? [String: String] {
+                    self.mealID = mealID
+                
+                let items = mealItems.compactMap({ (arg: (key: String, value: String)) -> MealItems? in
+                    let mealItem = MealItems(id: arg.key, name: arg.value)
+                    return mealItem
+                })
+                
+                self.foodList = items
+                self.mealType = mealType
+                completion()
             }
         }
     }
