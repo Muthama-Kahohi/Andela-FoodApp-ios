dc2c9850ad8e7fb26374a36902b79cbcc27917dc
mungaiandela <martin.mungai@andela.com>
2018-04-23 11:52:45 +0300

    Create temporary date

--- Source/Core/ViewModel/MealsViewModel.swift
+++ Source/Core/ViewModel/MealsViewModel.swift
@@ -16,9 +16,9 @@ public class MealsViewModel {
     public func populateFoodList(completion: @escaping () -> Void) {
         
         let mealsRef = "meals/"
-//        let today = Date()
-//        let date = today.dateToIsoFormat(date: today)
-        let date = "2018-03-09" // temporary date
+        //        let today = Date()
+        //        let date = today.dateToIsoFormat(date: today)
+        let date = "2018-04-17" // temporary date
         
         guard
             let mealType = uniqueFetchID else { return }
@@ -29,16 +29,16 @@ public class MealsViewModel {
             if let mealTypeDict = snapshot.value as? [String: Any],
                 let mealID = mealTypeDict["meal_id"] as? String,
                 let mealItems = mealTypeDict["meal_items"] as? [String: String] {
-                    self.mealID = mealID
+                self.mealID = mealID
                 
-                    let items = mealItems.compactMap({ (arg: (key: String, value: String)) -> MealItems? in
-                        let mealItem = MealItems(id: arg.key, name: arg.value)
-                        return mealItem
-                    })
+                let items = mealItems.compactMap({ (arg: (key: String, value: String)) -> MealItems? in
+                    let mealItem = MealItems(id: arg.key, name: arg.value)
+                    return mealItem
+                })
                 
-                    self.foodList = items
-                    self.mealType = mealType
-                    completion()
+                self.foodList = items
+                self.mealType = mealType
+                completion()
             }
         }
     }
